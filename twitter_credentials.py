dbef7867a9da1fdb8f0ef64c182fd3167b1205be
Joey Garcia <111665680+YouKnowJoey@users.noreply.github.com>
2025-05-03 23:54:35 -0700

    Create twitter_credentials.py
    skeleton for Twitter credentials, this will be created into a class to use in a Python notebook.

---
+++ twitter_credentials.py
@@ -0,0 +1,15 @@
+"""
+Credits:
+
+Project uses code from [@vprusso](https://github.com/vprusso)'s repository: [YouTube Tuturials/ Twitter Sentiment Analysis](https://github.com/vprusso/youtube_tutorials/blob/master/twitter_python/part_5_sentiment_analysis_tweet_data/sentiment_anaylsis_twitter_data.py).  
+Thank you [@vprusso](https://github.com/vprusso) for your contribution to the open-source community.
+"""
+
+# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
+# Variables that contains the user credentials to access Twitter API
+
+### Credentials are unique (sacred) to every user ###
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
